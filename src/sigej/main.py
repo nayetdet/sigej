3ba01b9e63763f4793f188295879f0f83fbf2287
nayetdet <jpmoreiracampos21@gmail.com>
2025-11-24 15:21:45 +0000

    refactor: renomeados arquivos de todos os routes

--- src/sigej/main.py
+++ src/sigej/main.py
@@ -1,32 +1,32 @@
 from flask import Flask
 from src.sigej.config import Config
-from src.sigej.routes.home import bp as home_bp
-from src.sigej.routes.pessoas import bp as pessoas_bp
-from src.sigej.routes.ordem_servico import bp as os_bp
-from src.sigej.routes.solicitantes import bp as solicitantes_bp
-from src.sigej.routes.areas import bp as areas_bp
-from src.sigej.routes.tipos_area import bp as tipos_area_bp
-from src.sigej.routes.tipos_os import bp as tipos_os_bp
-from src.sigej.routes.equipes import bp as equipes_bp
-from src.sigej.routes.funcionarios import bp as funcionarios_bp
-from src.sigej.routes.setores import bp as setores_bp
-from src.sigej.routes.tipos_funcionario import bp as tipos_funcionario_bp
-from src.sigej.routes.cadastros_index import bp as cadastros_index_bp
-from src.sigej.routes.categorias import bp as categorias_bp
-from src.sigej.routes.unidades_medida import bp as unidades_bp
-from src.sigej.routes.marcas import bp as marcas_bp
-from src.sigej.routes.fornecedores import bp as fornecedores_bp
-from src.sigej.routes.cores import bp as cores_bp
-from src.sigej.routes.tamanhos import bp as tamanhos_bp
-from src.sigej.routes.produtos import bp as produtos_bp
-from src.sigej.routes.produto_variacoes import bp as variacoes_bp
-from src.sigej.routes.locais_estoque import bp as locais_estoque_bp
-from src.sigej.routes.tipos_movimento import bp as tipos_movimento_bp
-from src.sigej.routes.status_os import bp as status_os_bp
-from src.sigej.routes.equipe_membros import bp as equipe_membros_bp
-from src.sigej.routes.itens_os import bp as itens_os_bp
-from src.sigej.routes.andamentos_os import bp as andamentos_os_bp
-from src.sigej.routes.movimentos_estoque import bp as movimentos_bp
+from src.sigej.routes.home_route import bp as home_bp
+from src.sigej.routes.pessoas_route import bp as pessoas_bp
+from src.sigej.routes.ordem_servico_route import bp as os_bp
+from src.sigej.routes.solicitantes_route import bp as solicitantes_bp
+from src.sigej.routes.areas_route import bp as areas_bp
+from src.sigej.routes.tipos_area_route import bp as tipos_area_bp
+from src.sigej.routes.tipos_os_route import bp as tipos_os_bp
+from src.sigej.routes.equipes_route import bp as equipes_bp
+from src.sigej.routes.funcionarios_route import bp as funcionarios_bp
+from src.sigej.routes.setores_route import bp as setores_bp
+from src.sigej.routes.tipos_funcionario_route import bp as tipos_funcionario_bp
+from src.sigej.routes.cadastros_index_route import bp as cadastros_index_bp
+from src.sigej.routes.categorias_route import bp as categorias_bp
+from src.sigej.routes.unidades_medida_route import bp as unidades_bp
+from src.sigej.routes.marcas_route import bp as marcas_bp
+from src.sigej.routes.fornecedores_route import bp as fornecedores_bp
+from src.sigej.routes.cores_route import bp as cores_bp
+from src.sigej.routes.tamanhos_route import bp as tamanhos_bp
+from src.sigej.routes.produtos_route import bp as produtos_bp
+from src.sigej.routes.produto_variacoes_route import bp as variacoes_bp
+from src.sigej.routes.locais_estoque_route import bp as locais_estoque_bp
+from src.sigej.routes.tipos_movimento_route import bp as tipos_movimento_bp
+from src.sigej.routes.status_os_route import bp as status_os_bp
+from src.sigej.routes.equipe_membros_route import bp as equipe_membros_bp
+from src.sigej.routes.itens_os_route import bp as itens_os_bp
+from src.sigej.routes.andamentos_os_route import bp as andamentos_os_bp
+from src.sigej.routes.movimentos_estoque_route import bp as movimentos_bp
 
 def create_app() -> Flask:
     app = Flask(
